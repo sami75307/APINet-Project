470ce01e8e88887a5c9b68754531909c302cdd6f
sami75307 <84537182+sami75307@users.noreply.github.com>
2022-06-17 14:37:37 +0300

    Third part

---
+++ API/Services/TokanSevrvice.cs
@@ -0,0 +1,13 @@
+using API.Entites;
+using API.interfaces;
+
+namespace API.Services
+{
+    public class TokanSevrvice : ITokanSercivce
+    {
+        public string CreateToken(AppUser user)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
